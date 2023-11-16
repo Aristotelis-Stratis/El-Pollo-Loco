bb67f7fdd6fd31537fb9959be9e98a93ed95356f
Aristotelis-Stratis <stratis.dev@outlook.com>
2023-11-16 14:55:19 +0100

    constructor

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,4 +1,7 @@
 class Chicken extends MoveableObject {
 
+    constructor() {
+        super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+    }
 
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -3,6 +3,11 @@ class MoveableObject {
     y = 400;
     img;
     
+    loadImage(path){
+        this.img = new Image();
+        this.img.src = path;
+    }
+
 
     moveRight() {
         console.log('Moving right');
