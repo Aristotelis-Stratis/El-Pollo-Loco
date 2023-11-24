47c306a6d18916f78cc5f3290b7b7de1dc642cb1
Aristotelis-Stratis <stratis.dev@outlook.com>
2023-11-24 13:45:20 +0100

    sound effect on movement with optional pause effect

--- models/character.class.js
+++ models/character.class.js
@@ -11,6 +11,7 @@ class Character extends MoveableObject {
         'img/2_character_pepe/2_walk/W-26.png'
     ];
     world;
+    walking_sound = new Audio('audio/running.mp3');
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
@@ -25,10 +26,13 @@ class Character extends MoveableObject {
             if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.x += this.speed;
                 this.otherDirection = false;
+                this.walking_sound.play();
             }
             if (this.world.keyboard.LEFT && this.x > 0) {
                 this.x -= this.speed;
                 this.otherDirection = true;
+                this.walking_sound.pause();
+
             }
             this.world.camera_x = -this.x + 100;
         }, 1000 / 60);
