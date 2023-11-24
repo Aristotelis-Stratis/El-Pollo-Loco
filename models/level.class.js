d62cb707d9c96f43045af3f33060476682dcc2af
Aristotelis-Stratis <stratis.dev@outlook.com>
2023-11-24 13:21:21 +0100

    implemented level class and level-1 file

---
+++ models/level.class.js
@@ -0,0 +1,11 @@
+class Level {
+    enemies;
+    clouds;
+    backgroundObjects;
+
+    constructor(e, c, bo) {
+        this.enemies = e;
+        this.clouds = c;
+        this.backgroundObjects = bo;
+    }
+}
--- models/world.class.js
+++ models/world.class.js
@@ -1,20 +1,9 @@
 class World {
 
     character = new Character();
-    enemies = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-    ];
-    clouds = [
-        new Cloud()
-    ];
-    backgroundObjects = [
-        new BackgroundObject('img/5_background/layers/air.png', 0),
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 0),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
-    ];
+    enemies = level1.enemies;
+    clouds = level1.clouds;
+    backgroundObjects = level1.backgroundObjects;
     canvas;
     ctx;
     keyboard;
@@ -27,6 +16,8 @@ class World {
         this.draw();
         this.setWorld();
     }
+
+    
 
     setWorld() {
         this.character.world = this;
