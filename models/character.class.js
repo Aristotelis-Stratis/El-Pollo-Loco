c22393be9b3bc5d5a1ecbd6f88213a115b637ff5
Aristotelis-Stratis <stratis.dev@outlook.com>
2023-11-25 15:27:33 +0100

    collision frames

--- models/character.class.js
+++ models/character.class.js
@@ -24,7 +24,7 @@ class Character extends MoveableObject {
     ];
     world;
     walking_sound = new Audio('audio/running.mp3');
-    
+
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
@@ -38,18 +38,18 @@ class Character extends MoveableObject {
     animate() {
         setInterval(() => {
             if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
-                this.x += this.speed;
+                this.moveRight();
                 this.otherDirection = false;
                 this.walking_sound.play();
             }
             if (this.world.keyboard.LEFT && this.x > 0) {
-                this.x -= this.speed;
+                this.moveLeft();
                 this.otherDirection = true;
                 this.walking_sound.pause();
             }
 
             if (this.world.keyboard.SPACE && !this.isAboveGround()) {
-                this.speedY = 30;
+                this.jump();
             }
             this.world.camera_x = -this.x + 100;
         }, 1000 / 60);
@@ -66,9 +66,4 @@ class Character extends MoveableObject {
         }, 50);
     }
 
-
-    jump() {
-
-
-    }
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -12,19 +12,30 @@ class MoveableObject {
     acceleration = 2;
     applyGravity() {
         setInterval(() => {
-            if (this.isAboveGround()|| this.speedY > 0)
+            if (this.isAboveGround() || this.speedY > 0)
                 this.y -= this.speedY;
             this.speedY -= this.acceleration;
         }, 1000 / 25);
     }
 
-    isAboveGround(){
-       return this.y < 150;
+    isAboveGround() {
+        return this.y < 150;
     }
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
+    }
+
+    draw(ctx){
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+    drawFrame(ctx){
+        ctx.beginPath();
+        ctx.lineWidth = '3';
+        ctx.strokeStyle = 'blue';
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.stroke();
     }
 
     loadImages(array) {
@@ -44,12 +55,14 @@ class MoveableObject {
     }
 
     moveRight() {
-        console.log('Moving right');
+        this.x += this.speed;
     }
 
     moveLeft() {
-        setInterval(() => {
-            this.x -= this.speed;
-        }, 1000 / 60);
+        this.x -= this.speed;
+    }
+
+    jump() {
+        this.speedY = 30;
     }
 }
